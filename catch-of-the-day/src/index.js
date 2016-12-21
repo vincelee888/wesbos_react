31fcfd6bd0b00233f89b9779011a7a55dc4d2bb0
Vince Lee <vslee888@gmail.com>
2016-12-21 18:07:26 +0000

    import css

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './css/style.css' // import a compiled css file directly into react
 
 import StorePicker from './components/StorePicker'
 

e7e44ea0536b60d2f5757f64d2aaa8bbf7cde1f5
Vince Lee <vslee888@gmail.com>
2016-12-21 18:47:16 +0000

    routing

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = (props) => {
+  return (
+    <header>
+      <h1>Not found</h1>
+    </header>
+  )
+}
+
+export default NotFound
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router'
+
 import './css/style.css' // import a compiled css file directly into react
 
 import App from './components/App'
+import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
 
-render(<App/>, document.querySelector('#main'))
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} /> {/* any js variable must be in mustache tags */}
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Router />, document.querySelector('#main'))

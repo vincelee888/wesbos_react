5d060de41522fe5bddb6048963756971620dc5d9
Vince Lee <vslee888@gmail.com>
2016-12-21 19:24:49 +0000

    binding DOM events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import { getFunName } from '../helpers'
+
 class StorePicker extends React.Component {
-  render() {
+  // USE: if component is rendered multiple times to the page
+  // constructor () {
+  //   super()
+  //   this.goToStore = this.goToStore.bind(this) // wtf - sets 'this' scope
+  // }
+  // OR: bind within onSubmit:
+  // <form className="store-selector" onSubmit={ this.goToStore.bind(this) }>
+  // OR: arrow function; overhead of creating anonymous function each time the component is rendered
+  // <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }>
+
+  goToStore (e) {
+    e.preventDefault()
+    console.log(this.storeId.value) // this.storeId is the input element
+  } 
+
+  render () {
     return (
-      <form className="store-selector"> {/* 'class' is a reserved word in js; can only return a single dom element */}
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }> {/* 'class' is a reserved word in js; can only return a single dom element */}
         <legend>Please enter a store</legend>
-        <input type="text" required placeholder="Store name" /> {/* must close tags */}
+        <input type="text" required placeholder="Store name" defaultValue={ getFunName() } ref={ (v) => this.storeId = v } /> {/* must close tags */}
         <button type="submit">Visit Store</button>
       </form>
     ) // jsx alternative to React.createElement('p', blah blah blah)

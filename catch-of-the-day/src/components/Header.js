f74e2673b2ca5699ee58928b81e43874fce7fbf3
Vince Lee <vslee888@gmail.com>
2016-12-21 18:39:27 +0000

    use stateless functional component for components that only implement the render() function

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    console.log(this)
-    console.log('You can debug this in dev tools by selecting the Component in React Dev Tools, then entering "$r" in the Console')
-    console.log('You can debug any tag by selecting the Component in Elements, then entering "$0" in the Console')
-    return (
-      <header>
-        <h1>Catch of the Day</h1>
-        <h2>{this.props.tagline}</h2>
-      </header>
-    )
-  }
+// class Header extends React.Component {
+//   render() {
+//     console.log(this)
+//     console.log('You can debug this in dev tools by selecting the Component in React Dev Tools, then entering "$r" in the Console')
+//     console.log('You can debug any tag by selecting the Component in Elements, then entering "$0" in the Console')
+//     return (
+//       <header>
+//         <h1>Catch of the Day</h1>
+//         <h2>{this.props.tagline}</h2>
+//       </header>
+//     )
+//   }
+// }
+
+// just use a stateless functional component if only need the render
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Catch of the Day</h1>
+      <h2>{props.tagline}</h2>
+    </header>
+  )
 }
 
 export default Header

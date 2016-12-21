0c9d6ed0d4f4c38b0f18dd10bab3851ab222979e
Vince Lee <vslee888@gmail.com>
2016-12-21 18:35:20 +0000

    passing data with props; debugging components in console

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 class Header extends React.Component {
   render() {
+    console.log(this)
+    console.log('You can debug this in dev tools by selecting the Component in React Dev Tools, then entering "$r" in the Console')
+    console.log('You can debug any tag by selecting the Component in Elements, then entering "$0" in the Console')
     return (
-      <header>Feeesh</header>
+      <header>
+        <h1>Catch of the Day</h1>
+        <h2>{this.props.tagline}</h2>
+      </header>
     )
   }
 }

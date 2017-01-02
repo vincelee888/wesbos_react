b43555d1e2790e7fbac54cd2c8a998d7eab0dbac
Vince Lee <vslee888@gmail.com>
2017-01-02 11:26:54 +0000

    remove order item

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -18,6 +18,7 @@ class Order extends React.Component {
       <li key={key}>
         <span>{count}lbs of {fish.name}</span>
         <span className="price">{formatPrice(count * fish.price)}</span>
+        <button onClick={() => this.props.removeFromOrder(key)}>Remove</button>
       </li>
       )
     }
